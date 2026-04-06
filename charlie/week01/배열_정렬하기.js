0ccb7613d37b3209050c122405199663316f6c5c
yuncic <86828077+yuncic@users.noreply.github.com>
2026-04-06 17:12:17 +0900

    Revert "1주차 찰리 과제 제출합니다.(문제 1번~7번)"

--- charlie/week01/배열_정렬하기.js
+++
@@ -1,3 +0,0 @@
-function solution(arr) {
-    return arr.sort((a,b) => a-b);
-}
--- charlie/week01/배열_제어하기.js
+++
@@ -1,4 +0,0 @@
-function solution(arr){
-    const newArr = [...new Set(arr)];
-    return newArr.sort((a,b) => b-a);
-}
